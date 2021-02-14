de6d915af07e8e8599c0552c69015f43f75ed162
vanessuniq <vanessuniq@gmail.com>
2021-02-14 11:30:24 -0500

    types of literals variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // variables and literal
+    /**
+     * Java is a statically-type language. There's no hoisting concept here, meaning variables must be declared
+     * before being used. Each variable is assigned a data type when declared; and that data type cannot be change
+     * whitin the same scope. Also note that Java is case sensitive. best practice follows the camelCase nomenclature
+     */
+        // integer literals:
+        int age = 28;
+        System.out.println(age);
+        // floating-point literals
+        float myFloat = 3.4F;
+        double myDouble = 3.4;
+
+        System.out.println(myDouble + myFloat);
+
+        // character literal (unicode character in single quote)
+        char letter = 'V';
+        //String literal
+        String programming = "Java";
+
+        System.out.println(letter);
+        System.out.println(programming);
+
+        // boolean literals
+
+        boolean flag = false;
+        System.out.println(flag);
     }
+    
 }
