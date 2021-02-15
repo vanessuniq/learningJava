be559d51cbb963f991c5472881c39ec1ef929f04
vanessuniq <vanessuniq@gmail.com>
2021-02-14 23:01:08 -0500

    arithmetic operators

---
+++ Operators.java
@@ -0,0 +1,34 @@
+public class Operators {
+    public static void main(String[] args) {
+        /**
+         * We will be learning different types of operators in Java and how to use them.
+         * Operators are used to perform operations on variables and values. There are five types of Java operators:
+         * assignment, arithmetic, relational, logical, bitwise and unary operators.
+         * 
+         */
+
+        // Arithmetic Operators
+        /**
+         * 
+         *   Operator	Operation
+         * 
+             +	        Addition
+             -	        Subtraction
+             *	        Multiplication
+             /	        Division
+             %	        Modulo Operation (Remainder after division)
+         */
+        // the modulo operator is mainly used with int data type; and the division of two integers
+        // results in an integer. 
+
+         int a = 10, b = 8;
+
+         System.out.println("a + b = " + (a + b));
+         System.out.println("a - b = " + (a - b));
+         System.out.println("a * b = " + (a * b));
+         System.out.println("a / b = " + (a / b));
+         System.out.println("The remainder of a / b is " + (a % b));
+
+        
+    }
+}
