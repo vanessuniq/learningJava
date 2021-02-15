0b7006d127d53d18155a7753a23b3872dbfeba2d
vanessuniq <vanessuniq@gmail.com>
2021-02-15 11:03:22 -0500

    check conditions

--- FlowControl.java
+++ FlowControl.java
@@ -1,5 +1,14 @@
 public class FlowControl {
     public static void main(String[] args) {
+        int num = 8;
+        // if-else-if statement
+        if (num < 0) {
+            System.out.println("The variable num is a negative number");
+        } else if (num > 0) {
+            System.out.println("The variable num is a positive number");
+        } else {
+            System.out.println("The variable num is zero");
+        }
         
     }
 }
