e76fa5b0ecaaf176ea22d5893558b8b9aaaa68ad
vanessuniq <vanessuniq@gmail.com>
2021-02-15 09:58:45 -0500

    practice getting input

--- Input.java
+++ Input.java
@@ -5,8 +5,8 @@ public class Input {
     public static void main(String[] args) {
         //create a scanner object
         Scanner input = new Scanner(System.in);
-        // ask user for input
-        System.out.println("Enter an integer: ");
+        // ask user for integer input
+        System.out.print("Enter an integer: ");
         
         // read input
         int num = input.nextInt();
@@ -14,7 +14,19 @@ public class Input {
         // return the output to user
         System.out.println("You entered: " + num);
 
-        // close the input scanner
+        // ask for float input
+        System.out.print("Enter a float: ");
+        System.out.println("You entered: " + input.nextFloat());
+
+        // ask for double
+        System.out.print("Enter a double: ");
+        System.out.println("You entered: " + input.nextDouble());
+
+        // ask for string
+        System.out.print("What is your name? ");
+        System.out.println("Hey " + input.next() + "!");
+
+        // close the input scanner. This is recommended after capturing the inputs
         input.close();
 
     }
