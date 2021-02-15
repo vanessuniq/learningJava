c1ce9a925475372124e32233a777dd89f1a14df8
vanessuniq <vanessuniq@gmail.com>
2021-02-15 11:59:51 -0500

    swich statement

--- FlowControl.java
+++ FlowControl.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class FlowControl {
     public static void main(String[] args) {
         int num = 8;
@@ -10,5 +12,43 @@ public class FlowControl {
             System.out.println("The variable num is zero");
         }
         
+        // switch statement
+        char operator;
+        Double double1, double2, result = 0.0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Choose an operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Enter your first number: ");
+        double1 = input.nextDouble();
+
+        System.out.print("Enter your second number: ");
+        double2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = double1 + double2;
+                break;
+
+            case '-':
+                result = double1 - double2;
+                break;
+
+            case '*':
+                result = double1 * double2;
+                break;
+                
+            case '/':
+                result = double1/double2;
+                break;
+
+            default:
+                System.out.println("Unknown operator: thus, the following is not a valid operation.");
+                break;
+        }
+        System.out.println(double1 + " " + operator + " " + double2 + " = " + result);
+
+        input.close();
     }
 }
