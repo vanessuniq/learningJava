6fc29af6ecafa0b586b718d12396aa49016973b4
Vanessa Fotso <vfotso@mitre.org>
2022-04-18 14:58:01 -0400

    Arithmetic operation with Java

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperators {
+  public static void main(String[] args) {
+    int num1 = 29, num2 = 84, result;
+    result = num1 + num2;
+
+    System.out.println("num1 + num2 = " + result);
+    result = num2 - num1;
+    System.out.println("num2 - num1 = " + result);
+    result = num1 * num2;
+    System.out.println("num2 * num1 = " + result);
+    float res = (float) num2 / num1;
+    System.out.println("num2 / num1 = " + res);
+
+  }
+}
