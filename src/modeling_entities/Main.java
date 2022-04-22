7f1c40155d3759eb5dd4c6267b01b0713d52ae70
Vanessa Fotso <vfotso@mitre.org>
2022-04-22 13:14:25 -0400

    Using private fields, getters and setters as common practice (encapsulation)

--- src/modeling_entities/Main.java
+++ src/modeling_entities/Main.java
@@ -3,6 +3,6 @@ package modeling_entities;
 public class Main {
   public static void main(String[] args) {
     Student vanessa = new Student("Vanessa", 29, 3.59);
-    System.out.format("Hello, my name is %s, I am %d, and my GPA is %,.2f.\n", vanessa.name, vanessa.age, vanessa.gpa);
+    vanessa.printStudentInfo();
   }
 }
--- src/modeling_entities/Student.java
+++ src/modeling_entities/Student.java
@@ -2,13 +2,44 @@ package modeling_entities;
 
 public class Student {
   // Member variables (instance variables or fields)
-  public String name;
-  public int age;
-  public double gpa;
+  private String name;
+  private int age;
+  private double gpa;
 
   public Student(String name, int age, double gpa) {
     this.name = name;
     this.age = age;
     this.gpa = gpa;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public double getGpa() {
+    return gpa;
+  }
+
+  public String setName(String name) {
+    this.name = name;
+    return this.name;
+  }
+
+  public int setAge(int age) {
+    this.age = age;
+    return this.age;
+  }
+
+  public double setGpa(double gpa) {
+    this.gpa = gpa;
+    return this.gpa;
+  }
+
+  public void printStudentInfo() {
+    System.out.format("Hi, my name is %s and I am %d. My current GPA is %,.2f.\n", name, age, gpa);
   }
 }
