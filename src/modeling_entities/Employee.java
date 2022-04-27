dc412ccf4af816eb6ec39c449017fda4da183176
Vanessa Fotso <vfotso@mitre.org>
2022-04-26 22:27:08 -0400

    using private instance variable, getters and setters

---
+++ src/modeling_entities/Employee.java
@@ -0,0 +1,76 @@
+package modeling_entities;
+
+public class Employee {
+  private String name;
+  private double experience;
+  private double salary;
+  private String title;
+
+  public Employee(String name, String title, double experience, double salary) {
+    this.name = name;
+    this.title = title;
+    this.experience = experience;
+    this.salary = salary;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public double getExperience() {
+    return experience;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+  public void setExperience(double experience) {
+    this.experience = experience;
+  }
+
+  public void update(String name, String title, double experience, double salary) {
+    setName(name);
+    setTitle(title);
+    setExperience(experience);
+    setSalary(salary);
+  }
+
+  public double calculateBonus(float performancePercentage) {
+    double bonus = salary * performancePercentage * 0.01;
+    return bonus;
+  }
+
+  public double calculateIncrement() {
+    double increment = 0.05;
+    if (salary < 65000) {
+      increment = 0.10;
+    }
+    salary += salary * increment;
+    return increment;
+  }
+
+  public void printEmployeeInfo() {
+    System.out.format("%s is a %s with %.2f experience making $%.2f.\n", name, title, experience, salary);
+    System.out.format("After the performance review this year, %s received a bonus of $%.2f.\n", name,
+        calculateBonus(10));
+    System.out.format("Moreover, %s's salary increased by %.2f percent to $%.2f.", name, calculateIncrement() * 100,
+        salary);
+  }
+}
--- src/modeling_entities/Main.java
+++ src/modeling_entities/Main.java
@@ -4,5 +4,8 @@ public class Main {
   public static void main(String[] args) {
     Student vanessa = new Student("Vanessa", 29, 3.59);
     vanessa.printStudentInfo();
+
+    Employee matt = new Employee("Matt", "teacher", 3.5, 60000);
+    matt.printEmployeeInfo();
   }
 }
